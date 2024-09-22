64e68e650c395471133da0f656870ef8a37985a8
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-09-22 17:32:56 +0300

    compl storage posts

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let userData = [];
   let postIdArr = [];
   let postTextArr = [];
-
+  let postInfoArr = [];
 function idGenerate() {
     return Math.random().toString(16).slice(2);
   }
@@ -178,10 +178,16 @@ function idGenerate() {
     userData.forEach(user => {
       console.log(user.post(postText.value));
       postTextArr.push(postText.value)
-      infoPostText.innerHTML = `User ${user.name}, with ID ${user.id} posted: ${postText.value} (Post ID: ${postIdArr[postIdArr.length - 1]})`
-      console.log(`${postTextArr[postTextArr.length - 1]} with ID ${postIdArr[postIdArr.length - 1]}`); 
+      infoPostText.innerHTML = `User ${user.name},posted: ${postText.value} `
       
+      postInfoArr.push({
+        idPost: postIdArr[postIdArr.length - 1],
+        text: postTextArr[postTextArr.length - 1],
+        author: user.name,
+        userId: user.id})
+        
     })
+    console.log(postInfoArr);
     
   })  
 })
