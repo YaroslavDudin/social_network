d484abd6bcbdc6dc9a5c797048057d8e729f6980
YaroslavDudin <yaroslavedudin@yandex.ru>
2024-09-22 22:03:13 +0300

    add errors

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const postsButton = document.getElementById('post-button')
   postsButton.style.display = "none"
+// ERORS
+  const passwordError = document.getElementById('password-error')
+  const emailError = document.getElementById('email-error')
+  const ageError = document.getElementById('age-error')
+  const usernameError = document.getElementById('username-error')
+  const phoneError = document.getElementById('phone-error')
 
 const phoneInput = document.getElementById('phone');
   const usernameInput = document.getElementById('username');
@@ -79,9 +85,11 @@ function idGenerate() {
       const PHONE_REGEXP = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
       if (!PHONE_REGEXP.test(phoneInput.value)) {
         phoneInput.style.borderColor = 'red'
+        phoneError.innerHTML = "Phone number is not valid"
         return false
       }
       phoneInput.style.borderColor = 'green'
+      phoneError.innerHTML = ""
       return true
     }
     function checkPassword() {
@@ -91,73 +99,87 @@ function idGenerate() {
       const specialCharacters = /[!@#$%^&*()_+\-=\]{};':"\\|,.<>?]/g
       if (passwordInput.value.length < 8) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password must be at least 8 characters long"
         return false
         }
     
       if (passwordInput.value === emailInput.value) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password cannot be the same as email"
         return false
       }
     
       if (passwordInput.value.includes(' ')) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password cannot contain spaces"
         return false
       }
     
       if (!upperCaseLetters.test(passwordInput.value)) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password must contain at least one uppercase letter"
         return false
       }
     
       if (!lowerCaseLetters.test(passwordInput.value)) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password must contain at least one lowercase letter"
         return false
       }
     
       if (!numbers.test(passwordInput.value)) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password must contain at least one number"
         return false
       }
     
       if (!specialCharacters.test(passwordInput.value)) {
         passwordInput.style.borderColor = 'red'
+        passwordError.innerHTML = "Password must contain at least one special character"
         return false
       }
       passwordInput.style.borderColor = 'green'
+      passwordError.innerHTML = ""
       return true
     }
     function checkEmail() {
       const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
       if (emailInput.value.includes(' ')) {
         emailInput.style.borderColor = 'red'
+
         return false;
       }
       if (!EMAIL_REGEXP.test(emailInput.value)) {
         emailInput.style.borderColor = 'red'
+        emailError.innerHTML = "Email is not valid"
         return false;
       }
       if (userData.some(user => user.email === emailInput.value)) {
         emailInput.style.borderColor = 'red'
-        throw new Error("Email is already registered")
-
+        emailError.innerHTML = "Email is already registered"
       }
       emailInput.style.borderColor = 'green'
+      emailError.innerHTML = ""
       return true;
     }
     function checkAge() {
       if (ageInput.value < 14) {
         ageInput.style.borderColor = 'red'
-        throw new Error("Age must be greater than 14")
+        ageError.innerHTML = "Age must be greater than 14"
+        return false
       }
       ageInput.style.borderColor = 'green'
+      ageError.innerHTML = ""
         return true
    }
     function checkUsername() {
       if (usernameInput.value.length < 3) {
         usernameInput.style.borderColor = 'red'
-        throw new Error("Username must be at least 3 characters long")
+        usernameError.innerHTML = "Username must be at least 3 characters long"
+        return false
       }
       usernameInput.style.borderColor = 'green'
+      usernameError.innerHTML = ""
       return true
     }
     if(checkPassword() && checkEmail() && checkAge() && checkUsername() && checkPhone()) {
